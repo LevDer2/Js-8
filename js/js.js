d741186eaf9306358d90d6933c107372dee068e2
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-29 16:19:55 +0200

    yes

---
+++ js/js.js
@@ -0,0 +1,35 @@
+const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+let string = "";
+for (let i = 0; i < friends.length; i += 1) {
+    string += friends[i];
+    if (i < friends.length -1) {
+        string += (", ")
+    }
+};
+
+console.log(string);
+
+const easy = friends.join(", ")
+console.log(easy);
+
+const cards = [
+    'Карточка-1',
+    'Карточка-2',
+    'Карточка-3',
+    'Карточка-4',
+    'Карточка-5',
+  ];
+
+  console.log(cards);
+  const cardToRemove = 'Карточка-3';
+  const index = cards.indexOf(cardToRemove);
+  console.log(index);
+  cards.splice(index, 1)
+console.log(cards);
+const cardToInsert = 'Карточка-6';
+cards.push("Карточка-6");
+console.log(cards);
+const cardToUpdate = 'Карточка-4';
+const index2 = cards.indexOf(cardToUpdate);
+cards.splice(index2, 1, 'Карточка-3', 'Карточка-4');
+console.log(cards);
