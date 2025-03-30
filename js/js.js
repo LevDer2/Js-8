16cb8239eaf5934a5da04c582f44176ec2c64fc4
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-30 19:33:45 +0300

    b

--- js/js.js
+++ js/js.js
@@ -12,22 +12,31 @@ console.log(string);
 const easy = friends.join(", ")
 console.log(easy);
 
+// 2Працюємо з колекцією карток в trello. Метод splice() (можна використати інші методи)
+
 const cards = [
     'Карточка-1',
     'Карточка-2',
     'Карточка-3',
     'Карточка-4',
     'Карточка-5',
-  ];
+];
 
-  console.log(cards);
-  const cardToRemove = 'Карточка-3';
-  const index = cards.indexOf(cardToRemove);
-  cards.splice(index, 1)
-console.log(cards);
+
+//3. Видалити
+
+const cardToRemove = 'Карточка-3';
+const index = cards.indexOf(cardToRemove);
+cards.splice(index, 1)
+
+
+//4. Додати
 const cardToInsert = 'Карточка-6';
 cards.push("Карточка-6");
-console.log(cards);
+console.log(cards);;
+
+
+//5Оновити
 const cardToUpdate = 'Карточка-4';
 const index2 = cards.indexOf(cardToUpdate);
 cards.splice(index2, 1, 'Карточка-3', 'Карточка-4');
